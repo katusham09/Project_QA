1a17fe2fdb67b4a83c80cad7176ee6d34293775e
Ekaterina <katusham0912@gmail.com>
2025-01-30 20:21:57 +0700

    Work with BD and Allure

--- src/test/java/autotests/tests/duckActionController/PropertiesTest.java
+++ src/test/java/autotests/tests/duckActionController/PropertiesTest.java
@@ -6,31 +6,41 @@ import autotests.payloads.WingState;
 import com.consol.citrus.TestCaseRunner;
 import com.consol.citrus.annotations.CitrusResource;
 import com.consol.citrus.annotations.CitrusTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 
+import java.util.Random;
+
+import static com.consol.citrus.container.FinallySequence.Builder.doFinally;
+
+@Epic("Тесты на duck-action-controller")
+@Feature("Эндпоинт /api/duck/action/properties")
 public class PropertiesTest extends DuckActionsClient {
     @Test(description = "Проверка получения характеристик уточки с четным id и material = wood")
     @CitrusTest
     public void checkDuckWithEvenIdr(@Optional @CitrusResource TestCaseRunner runner) {
-        Duck duck = new Duck().color("yellow").height(0.03).material("wood").sound("quack").wingsState(WingState.ACTIVE);
-        createDuck(runner, duck);
-        getDuckId(runner);
+        int id = new Random().nextInt(1000000);
+        runner.variable("duckId",id);
+        createDuck(runner, "${duckId}", "yellow", "0.03", "wood", "quack", "ACTIVE");
         oddParityCheck(runner, "${duckId}", "wood");
         propertiesDuck(runner, "${duckId}");
+        Duck duck = new Duck().id("@ignore@").color("yellow").height(0.03).material("wood").sound("quack").wingsState(WingState.ACTIVE);
         validateResponse(runner, duck);
-        deleteDuck(runner, "${duckId}");
+        runner.$(doFinally().actions(deleteDuckBD(runner, "${duckId}")));
     }
 
     @Test(description = "Проверка получения характеристик уточки с нечетным id и material = rubber")
     @CitrusTest
     public void checkDuckWithOddIdr(@Optional @CitrusResource TestCaseRunner runner) {
-        Duck duck = new Duck().color("yellow").height(0.03).material("rubber").sound("quack").wingsState(WingState.ACTIVE);
-        createDuck(runner, duck);
-        getDuckId(runner);
+        int id = new Random().nextInt(1000000);
+        runner.variable("duckId",id);
+        createDuck(runner, "${duckId}", "yellow", "0.03", "rubber", "quack", "ACTIVE");
         parityCheck(runner, "${duckId}", "rubber");
         propertiesDuck(runner, "${duckId}");
+        Duck duck = new Duck().color("yellow").height(0.03).material("rubber").sound("quack").wingsState(WingState.ACTIVE);
         validateResponse(runner, duck);
-        deleteDuck(runner, "${duckId}");
+        runner.$(doFinally().actions(deleteDuckBD(runner, "${duckId}")));
     }
 }
